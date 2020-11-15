b237fbe347bf2534b02f35d30489ed2a8b35eaec
Yevgeniy <anigeka@gmail.com>
2020-11-15 12:56:06 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_reader :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = [] unless @roster[grade].kind_of?(Array)
+        @roster[grade] << name
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = Hash.new
+        @roster.each do |grade, students|
+            sorted_hash[grade] = students.sort 
+        end
+        sorted_hash
+    end
+end 
